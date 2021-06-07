d58209925c643dca618074c3839815adc2c7536a
Angel Trevino <angel.trevinov@gmail.com>
2021-06-07 18:05:21 -0500

    adding initial stuff

---
+++ pokeblock-client/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="navbar navbar-dark bg-dark sticky-top" style="margin-bottom: 10px;">
+    <div class="container-fluid">
+      <a class="navbar-brand mb-0 h1" href="#">PokeBlock</a>
+    </div>
+  </nav>
+  <CreateAccount></CreateAccount>
+</template>
+
+<script>
+import CreateAccount from "./components/CreateAccout";
+
+export default {
+  name: 'App',
+  components: {
+    CreateAccount
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ pokeblock-client/src/components/CreateAccout.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col text-center">
+        <h3>Escoge a Tu Pokemon inicial!</h3>
+      </div>
+    </div>
+    <div class="row">
+      <div v-for="pokemon in starters" :key="pokemon.id" class="col d-flex justify-content-center">
+        <PokemonHolder v-bind:pokemon="pokemon"></PokemonHolder>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import Constants from "../Constants";
+import PokemonHolder from "./PokemonHolder";
+
+export default {
+  name: "CreateAccount",
+  components: {
+    PokemonHolder
+  },
+  data() {
+    return {
+      starters: []
+    }
+  },
+  mounted() {
+    this.getPokemonStarters(Constants.Bulbasaur);
+    this.getPokemonStarters(Constants.Charmander);
+    this.getPokemonStarters(Constants.Squirtle);
+  },
+  methods: {
+    getPokemonStarters(id) {
+      axios.get(Constants.pokeApi + "/" + id)
+          .then(response => {
+            var pokemon = {
+              "name": response.data.name,
+              "id": response.data.id,
+              "img": response.data.sprites.front_default,
+              "type": response.data.types[0].type.name
+            };
+            this.starters.push(pokemon);
+          });
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
